8ced8da4eea4cc57b5005be25c5be997844fcbf0
Himanshu_Chandra <himanshu.chandra@outlook.com>
2017-07-03 17:00:32 +0530

    links

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -27,8 +27,18 @@ angular.module('novusApp')
 
       var promise=dashboard.loadData();
       promise.then(function(data){
+        // console.log(data);
         if(data.data!=undefined){
           $scope.cases=data.data;
+          for(var i=0;i<data.data.length;i++){
+            var judgements=data.data[i].judgements;
+            var jArray=judgements.split(',');
+            data.data[i].jArray=jArray;
+          }
+          
+          // var zz=judgements.split(',');
+          console.log(data.data);
+
         }
         else{
           $scope.dashboardMessage="No cases yet";
