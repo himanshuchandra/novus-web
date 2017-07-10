3cdcd8da0ae8b91093a8e87c39f65bffdd504c4d
Mukul Juneja <junejamukul7@gmail.com>
2017-07-10 11:53:03 +0530

    calender view added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,6 +27,11 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
+      .when('/calender', {
+        templateUrl: 'views/calender.html',
+        controller: 'calender',
+        controllerAs: 'calender'
+      })
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl',
---
+++ app/scripts/controllers/calender.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name novusApp.controller:Calender
+ * @description
+ * # Calender
+ * Controller of the novusApp
+ */
+angular.module('novusApp')
+  .controller('calender', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
